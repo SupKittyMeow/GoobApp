1ac99661ca7b557167146e2cb849f061b32976dd
SupKittyMeow <77246128+SupKittyMeow@users.noreply.github.com>
2025-11-24 19:54:52 -0800

    asdf

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const URL = process.env.NODE_ENV === 'production' ? 'goobapp-backend.koyeb.app/' : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' ? 'https://goobapp-backend.koyeb.app/' : 'http://localhost:3000';
 export const socket = io(URL, {
     autoConnect: true // TODO: make this false, and only connect once logged in
 });
