74e563cdd2064b3bae1b8549222eb4601fb53e4d
SupKittyMeow <supkittymeow@gmail.com>
2025-11-26 07:01:22 -0800

    include the public link thing + slightly improve profile view

--- src/components/supabase/Client.tsx
+++ src/components/supabase/Client.tsx
@@ -1,7 +1,7 @@
 // Followed tutorial: https://mobisoftinfotech.com/resources/blog/app-development/supabase-react-typescript-tutorial
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseUrl = "https://wfdcqaqihwsilzegcknq.supabase.co";
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
 export const Client = createClient(supabaseUrl, supabasePublishableKey);
